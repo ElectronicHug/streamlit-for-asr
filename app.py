8fdc37b81dce171d01f4cdcbd66c307a381a4142
ElectronicHug <43905681+ElectronicHug@users.noreply.github.com>
2024-05-10 19:51:32 +0300

    init

---
+++ app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import os
+import requests
+
+def list_audio_files(directory):
+    """List mp3 files in a specified directory."""
+    files = [f for f in os.listdir(directory) if f.endswith('.mp3')]
+    return files
+
+def send_audio_to_api(audio_file):
+    """Function to send an audio file to your API."""
+    url = 'https://mozila-med-flask-vad2a77hmq-ey.a.run.app/upload'  # Replace with your actual API URL
+    files = {'file': open(audio_file, 'rb')}
+    try:
+        response = requests.post(url, files=files)
+        # st.write(response.text)
+        if response.status_code == 400:
+            return {"error": "No file was uploaded. Please upload an audio file."}
+        elif response.status_code == 429:
+            return {"error": "API can handle only 1 request at a time. Please try again in 10 seconds."}
+        elif response.status_code != 200:
+            # Handle other HTTP errors
+            return {"error": f"Failed to process audio. Server responded with status code: {response.status_code}"}
+        return response.json()  # Assuming your API returns JSON and the response was successful
+    except requests.RequestException as e:
+        # Handle requests exceptions such as connection errors
+        return {"error": f"An error occurred while sending audio to the API: {str(e)}"}
+    finally:
+        files['file'].close()
+
+# Title of your app
+st.title('Audio File Processing App')
+
+# Audio file selection
+audio_folder = 'data'
+audio_files = list_audio_files(audio_folder)
+selected_file = st.selectbox('Choose an audio file:', audio_files)
+
+# External link to record audio
+st.markdown('Or [record your own audio here](https://online-voice-recorder.com/)', unsafe_allow_html=True)
+
+# File uploader for user recordings
+uploaded_file = st.file_uploader("Or upload your audio file here (.mp3):", type="mp3")
+if uploaded_file is not None:
+    with open(os.path.join(audio_folder, uploaded_file.name), "wb") as f:
+        f.write(uploaded_file.getvalue())
+
+
+if uploaded_file is not None:
+    # Use the uploaded file
+    audio_data = uploaded_file.getvalue()
+    audio_file_to_display = uploaded_file.name
+else:
+    # Use the selected file from the dropdown
+    audio_file_to_display = os.path.join(audio_folder, selected_file)
+    audio_data = open(audio_file_to_display, "rb").read()
+    
+file_to_send = uploaded_file.name if uploaded_file is not None else selected_file
+if st.button('Send Audio to API'):
+    st.audio(audio_data)
+    st.markdown(f"""
+        ### Sending Audio to API
+        - **Selected file:** `{file_to_send}`
+        - **Processing time:**
+            - Typically, this will take about **20 seconds**.
+            - If it's a cold start, it could take up to **1 minute**.
+    """)
+    response = send_audio_to_api(os.path.join(audio_folder, file_to_send))
+    if 'error' in response:
+        st.error(response['error'])
+    else:
+        st.markdown(f"""
+            ### Response from API
+            > {response['transcript']}
+        """)
